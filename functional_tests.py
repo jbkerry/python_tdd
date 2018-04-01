35a54c88c2177eedb7a38d4c4b2dd4ff30b014d8
Jon Kerry <jonbkerry@gmail.com>
2018-04-01 11:43:26 +0100

    Refactor of table items test

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('cargo_list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Roger has heard about a cool new fantasy trade game. He goes to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists "1: Grain" as an item of cargo
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('cargo_list')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Grain', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Grain')
 
         # There is still a text box inviting him to add another type of cargo. He enters "Iron".
         inputbox = self.browser.find_element_by_id('new_cargo')
@@ -48,10 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both types of cargo on his list
-        table = self.browser.find_element_by_id('cargo_list')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Grain', [row.text for row in rows])
-        self.assertIn('2: Iron', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Grain')
+        self.check_for_row_in_list_table('2: Iron')
 
         # Roger wonders whether the site will remember his list. Then he sees that the site has generated a unique URL
         # for him -- there is some explanatory text to that effect.
