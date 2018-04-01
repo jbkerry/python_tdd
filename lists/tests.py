a353a5463e5c87fe76049611f30be712672037a3
Jon Kerry <jonbkerry@gmail.com>
2018-04-01 12:08:18 +0100

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from .models import Item
 from django.test import TestCase
 
 
@@ -11,3 +12,23 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'cargo_text': 'A new cargo item'})
         self.assertIn('A new cargo item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) cargo item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Cargo the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) cargo item')
+        self.assertEqual(second_saved_item.text, 'Cargo the second')
